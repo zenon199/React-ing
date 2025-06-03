f3694f827ca62dc4e6c55a7676cd0e6bc9574a1b
Zenon <abhishekyadav276208@gmail.com>
2025-06-03 20:38:45 +0530

    Revert "done"
    This reverts commit 3ac19187b524aed16fc3c9c65b904a5c745e9758.

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,75 +1,35 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
-type Todo = {
-    id: number,
-    text: string,
-    isCompleted: boolean
-  }
-
 function App() {
-  
-  const [input, setInput] = useState("");
-  const [list, setList] = useState<Todo[]>([]);
+  const [count, setCount] = useState(0)
 
-  const addTodo = () => {
-
-    if (input.trim() === "" || list.find(t => t.text === input)) {
-      alert("Enter a valid todo");
-      setInput("")
-      return;
-    }
-  
-    const item: Todo = {
-      id: list.length + 1,
-      text: input.trim(),
-      isCompleted: false
-    }
-
-    setList(prev => [...prev, item]);
-    setInput("");
-  }
-
-  const toggleCompleted = (id :number) => {
-    setList(
-      list.map(t => {
-        if (t.id === id) {
-          return {
-            ...t,
-            isCompleted: !t.isCompleted
-          }
-        } else return t; 
-      })
-    )
-  }
-
-  const deleteTodo = (id: number) => {
-    setList(
-      list.filter(
-        (t) => t.id !== id
-      )
-    )
-  }
-
-  return (<>
+  return (
+    <>
       <div>
-      <input type="text" placeholder='Enter your Todos' value={input} onChange={(e) => setInput(e.target.value)} />
-      <span style={{padding:20}}>
-        <button onClick={() => addTodo()}>Add</button>
-      </span>
-
-      <ul>
-        {list.map(l => 
-          <li key={l.id}>
-            <input type="checkbox" onChange={() =>toggleCompleted(l.id)}/>
-            <span className={l.isCompleted ? 'strikeThrough' : ''}>{l.text}</span>
-            <button style={{marginLeft: 20}} onClick={() => deleteTodo(l.id)}>Delete</button>
-          </li>
-        )}
-      </ul>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-)
+  )
 }
 
 export default App
